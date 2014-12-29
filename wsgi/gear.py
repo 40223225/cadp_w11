05e191ecbef25cb3b2347ac4cd55af59f5f660be
40223225 <40223225@gm.nfu.edu.tw>
2014-12-29 18:42:21 +0800

    add

--- wsgi/gear.py
+++ wsgi/gear.py
@@ -344,7 +344,7 @@ class Gear(object):
     // ]]></script>
     <script type="text/javascript" language="JavaScript">// <![CDATA[
     if (!pfcIsWindows()) netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
-    // 若第三輸入為 false, 表示僅載入 session, 但是不顯示
+    // 若第三輸入為 false, 表示僅載入 session, 但是 不顯示
     // ret 為 model open return
      var ret = document.pwl.pwlMdlOpen("gear.prt", "v:/", false);
     if (!ret.Status) {
